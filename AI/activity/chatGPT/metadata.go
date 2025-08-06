1de0b813cea4b930cd4221e2bc31decb44ce8245
mbloomfi-tibco <106804427+mbloomfi-tibco@users.noreply.github.com>
2025-08-06 03:46:13 +0200

    test

--- AI/activity/chatGPT/metadata.go
+++ AI/activity/chatGPT/metadata.go
@@ -6,40 +6,56 @@ type Settings struct {
 	ASetting string `md:"aSetting,required"`
 }
 
-// Input corresponds to activity.json inputs
+// // Input corresponds to activity.json inputs
+// type Input struct {
+// 	apiKey string `md:"apiKey,required"`
+// 	model  string `md:"model,required"`
+// 	prompt string `md:"prompt,required"`
+// }
+
+// // FromMap converts a map to Input struct
+// func (i *Input) FromMap(values map[string]interface{}) error {
+// 	var err error
+
+// 	i.apiKey, err = coerce.ToString(values["apiKey"])
+// 	if err != nil {
+// 		return err
+// 	}
+
+// 	i.model, err = coerce.ToString(values["model"])
+// 	if err != nil {
+// 		return err
+// 	}
+
+// 	i.prompt, err = coerce.ToString(values["prompt"])
+// 	if err != nil {
+// 		return err
+// 	}
+
+// 	return nil
+// }
+
+// func (r *Input) ToMap() map[string]interface{} {
+// 	return map[string]interface{}{
+// 		"apiKey": r.apiKey,
+// 		"model":  r.model,
+// 		"prompt": r.prompt,
+// 	}
+// }
+
 type Input struct {
-	apiKey string `md:"apiKey,required"`
-	model  string `md:"model,required"`
-	prompt string `md:"prompt,required"`
+	AnInput string `md:"anInput,required"`
 }
 
-// FromMap converts a map to Input struct
-func (i *Input) FromMap(values map[string]interface{}) error {
-	var err error
-
-	i.apiKey, err = coerce.ToString(values["apiKey"])
-	if err != nil {
-		return err
-	}
-
-	i.model, err = coerce.ToString(values["model"])
-	if err != nil {
-		return err
-	}
-
-	i.prompt, err = coerce.ToString(values["prompt"])
-	if err != nil {
-		return err
-	}
-
+func (r *Input) FromMap(values map[string]interface{}) error {
+	strVal, _ := coerce.ToString(values["anInput"])
+	r.AnInput = strVal
 	return nil
 }
 
 func (r *Input) ToMap() map[string]interface{} {
 	return map[string]interface{}{
-		"apiKey": r.apiKey,
-		"model":  r.model,
-		"prompt": r.prompt,
+		"anInput": r.AnInput,
 	}
 }
 
